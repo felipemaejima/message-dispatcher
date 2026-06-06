42135d295f080150cc3d6b0648206465baf197cd
Felipe Maejima <felipemaejima@gmail.com>
2026-06-06 00:12:05 -0300

    Feat: Iniciando o consumo de fila para disparo de mensagens

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.26.4
 
 require (
 	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/rabbitmq/amqp091-go v1.11.0 // indirect
 	github.com/resend/resend-go/v3 v3.7.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,21 +1,95 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"os"
 
-	"github.com/resend/resend-go/v3"
 	"github.com/joho/godotenv"
+	amqp "github.com/rabbitmq/amqp091-go"
+	"github.com/resend/resend-go/v3"
 )
 
+// todo adicionar o pacote do json para logs e playload
+type Email struct {
+	From    string
+	To      []string
+	Subject string
+	Bcc     []string
+	Cc      []string
+	ReplyTo string
+	Html    string
+	Text    string
+}
+
 func main() {
-	
+
 	err := godotenv.Load()
 
 	if err != nil {
 		panic(err)
 	}
 
+	brokerUrl := os.Getenv("BROKER_URL")
+
+	conn, err := amqp.Dial(brokerUrl)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer ch.Close()
+
+	msgs, err := ch.Consume(
+		"contact.messages", // nome da fila
+		"",                 // consumer name
+		true,               // auto ack
+		false,
+		false,
+		false,
+		nil,
+	)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Aguardando mensagens...")
+
+	for msg := range msgs {
+		log.Printf("Recebido: %s\n", msg.Body)
+	
+		// if err := processMessage(msg.Body); err != nil {
+		// 	msg.Nack(false, true)
+		// 	continue
+		// }
+	
+		// msg.Ack(false)
+	}
+
+	// _, err = dispatchEmail(Email{
+	// 	To: []string{
+	// 		"felipemaejima@gmail.com",
+	// 	},
+	// 	Subject: "Teste",
+	// 	Html:    "<h1>Hello</h1>",
+	// })
+
+	// if err != nil {
+	// 	log.Println(err)
+	// }
+}
+
+
+// todo email service
+func dispatchEmail(data Email) (string, error) {
 	apiKey := os.Getenv("RESEND_API_KEY")
 	emailDomain := os.Getenv("EMAIL_DOMAIN")
 
@@ -23,16 +97,16 @@ func main() {
 
 	params := &resend.SendEmailRequest{
 		From:    emailDomain,
-		To:      []string{"felipemaejima@gmail.com"},
-		Subject: "Teste",
-		Html:    "hELLO!",
+		To:      data.To,
+		Subject: data.Subject,
+		Html:    data.Html,
 	}
 
 	sent, err := client.Emails.Send(params)
 
 	if err != nil {
-		panic(err)
+		return "", err
 	}
 
-	fmt.Println(sent.Id)
+	return sent.Id, nil
 }
