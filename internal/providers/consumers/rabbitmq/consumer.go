63a36432d3e8a41dd282ee0bd3eacd4a8e7e2a65
Felipe Maejima <felipemaejima@gmail.com>
2026-06-06 13:28:34 -0300

    Feat: Adicionando integração com rabbitMQ

--- internal/providers/consumers/rabbitmq/consumer.go
+++ internal/providers/consumers/rabbitmq/consumer.go
@@ -1,1 +1,57 @@
 package rabbitmq
+
+import (
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+type Consumer struct {
+	conn  *amqp.Connection
+	ch    *amqp.Channel
+	queue string
+}
+
+func NewConsumer(
+	conn *amqp.Connection,
+	ch *amqp.Channel,
+	queue string,
+) *Consumer {
+
+	return &Consumer{
+		conn:  conn,
+		ch:    ch,
+		queue: queue,
+	}
+}
+
+func (c *Consumer) Consume(
+	handler func([]byte) error,
+) error {
+
+	msgs, err := c.ch.Consume(
+		c.queue,
+		"",
+		false,
+		false,
+		false,
+		false,
+		nil,
+	)
+
+	if err != nil {
+		return err
+	}
+
+	for msg := range msgs {
+
+		err := handler(msg.Body)
+
+		if err != nil {
+			msg.Nack(false, true)
+			continue
+		}
+
+		msg.Ack(false)
+	}
+
+	return nil
+}
